2438e5469a6db06613a03728fb99b3547fff6b2f
root <root@201011JUNC0383.(none)>
2015-03-05 15:00:21 +0530

    added string concatination

---
+++ addition.rb
@@ -0,0 +1,7 @@
+puts firstname = "ishu"
+puts last_name = "aruna"
+puts favourite_color = "blue"
+puts favouritecolor = "blue"
+gets first_n
+firstname = "ishu"
+puts "my name is firstname" + firstname
