6e577c5814a2fa424c5197ae20295e5e009dd7d0
root <root@201011JUNC0383.(none)>
2015-03-05 15:10:47 +0530

    removed unwanted line

--- addition.rb
+++ addition.rb
@@ -1,7 +1,6 @@
 puts firstname = "ishu"
 puts last_name = "aruna"
 puts favourite_color = "blue"
-puts favouritecolor = "blue"
 gets first_n
 firstname = "ishu"
 puts "my name is firstname" + firstname
